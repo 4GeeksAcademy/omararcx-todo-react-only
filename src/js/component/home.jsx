6f44a4ae19f166a92a67d94f3f3ae1a6070129c2
omararcx <100163833+omararcx@users.noreply.github.com>
2023-09-30 22:21:01 +0000

    created to do list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+let initialTarea = {
+	label: "",
+	done: false
+}
 
 //create your first component
 const Home = () => {
+
+	const [tarea, setTarea] = useState(initialTarea)
+	const [tareaLista, setTareaLista] = useState([])
+	const [error, setError] = useState(false)
+
+
+	const handleChange = (event) => {
+		setTarea({
+			label: event.target.value,
+			done: false
+		})
+	}
+
+
+	const handleSaveTask = (event) => {
+		if (event.key === "Enter") {
+			if (tarea.label.trim() !== "") {
+				setTareaLista([...tareaLista, tarea])
+				setTarea(initialTarea)
+				setError(false)
+			}
+			else {
+				setError(true)
+				console.log("Debes introducir una tarea valida")
+			}
+		}
+	}
+
+	const deleteTask = (id) => {
+		let newArr = tareaLista.filter((item, index) => index != id)
+		setTareaLista(newArr)
+
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="contenedor">
+			<h1>Lista de Tareas</h1>
+			{error ? <h3 className="errorMessage">Todos los campos son obligatorios</h3> : ""}
+			<form onSubmit={(event) => event.preventDefault()}>
+				<input className="textArea"
+					type="text"
+					value={tarea.label}
+					placeholder="Escribe una tarea"
+					onChange={handleChange}
+					onKeyDown={handleSaveTask}
+				>
+				</input>
+			</form>
+			<ol>
+				{tareaLista.map((item, index) => {
+					return <li key={index} onClick={() => deleteTask(index)}>{item.label}</li>
+
+				})}
+			</ol>
 		</div>
 	);
 };
